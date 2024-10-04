b029ab75cc09adf8eb6351747704630f0563683c
White Canvas <tfgwhitecanvas@gmail.com>
2024-10-03 23:53:28 -0300

    Tracking The UE5 GuidedTour Study project

---
+++ README.md
@@ -0,0 +1,3 @@
+# UE5GuidedTour
+
+Developed with Unreal Engine 5
---
+++ Source/UE5GuidedTour/UE5GuidedTour.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5GuidedTour.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5GuidedTour, "UE5GuidedTour" );
